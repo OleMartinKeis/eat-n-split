5c243c6987730dcc8e4376092d1111b9b67dce5f
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 10:43:31 +0100

    Added handlesubmit function

--- src/components/friendsList/addFriend/index.jsx
+++ src/components/friendsList/addFriend/index.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../button";
 
 function AddFriend() {
+    const [name, setName] = useState("");
+    const [img, setImg] = useState("https://i.pravatar.cc/48");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!name || !img) return;
+        const id = crypto.randomUUID();
+        const newFriend = {
+            name,
+            img: `${img}?=${id}`,
+            balance: 0,
+            id,
+        };
+        console.log(newFriend);
+        setName("");
+        setImg("https://i.pravatar.cc/48");
+    }
+
     return (
         <div>
-            <form className="form-add-friend">
+            <form className="form-add-friend" onSubmit={handleSubmit}>
                 <label>Friends name</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
                 <label>🖼️ Image URL</label>
-                <input type="text" />
+                <input
+                    type="text"
+                    value={img}
+                    onChange={(e) => setImg(e.target.value)}
+                />
                 <Button>Add</Button>
             </form>
         </div>
