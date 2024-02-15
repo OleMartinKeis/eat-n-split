fa2c66db10c7d5c601432d56dc6964a58c6134bf
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 10:43:41 +0100

    Added onclick prop to button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Button({ children }) {
-    return <button className="button">{children}</button>;
+function Button({ children, onClick }) {
+    return (
+        <button onClick={onClick} className="button">
+            {children}
+        </button>
+    );
 }
 
 export default Button;
