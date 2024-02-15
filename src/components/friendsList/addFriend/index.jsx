0c5d858e6590f691faf605d4c06277ee8afcc96a
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 08:37:21 +0100

    Added a add friend function

--- src/components/friendsList/addFriend/index.jsx
+++ src/components/friendsList/addFriend/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Button from "../../button";
 
 function AddFriend() {
-    return <div></div>;
+    return (
+        <div>
+            <form className="form-add-friend">
+                <label>Friends name</label>
+                <input type="text" />
+                <label>🖼️ Image URL</label>
+                <input type="text" />
+                <Button>Add</Button>
+            </form>
+        </div>
+    );
 }
 
 export default AddFriend;
