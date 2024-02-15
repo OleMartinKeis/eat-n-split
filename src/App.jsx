c31fa3b0810593bffe8c4fe9e5dbccc8fafff0a6
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 10:43:58 +0100

    Created handleShowAddFriend funciton

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "./components/button";
 import FriendsList from "./components/friendsList";
 import AddFriend from "./components/friendsList/addFriend";
@@ -5,12 +6,20 @@ import SplitBill from "./components/splitBill";
 import "./index.css";
 
 function App() {
+    const [showAddFriend, setShowAddFriend] = useState(false);
+
+    function handleShowAddFriend() {
+        setShowAddFriend(!showAddFriend);
+    }
+
     return (
         <div className="app">
             <div className="sidebar">
                 <FriendsList />
-                <AddFriend />
-                <Button>Add Friend</Button>
+                {showAddFriend && <AddFriend />}
+                <Button onClick={handleShowAddFriend}>
+                    {showAddFriend ? "Close" : "Add Friend"}
+                </Button>
             </div>
             <SplitBill />
         </div>
