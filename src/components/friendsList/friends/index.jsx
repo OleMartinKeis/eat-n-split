1082c9679a04c98b0327b67c5ce483cb2df94543
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 08:37:35 +0100

    Changed to custom button

--- src/components/friendsList/friends/index.jsx
+++ src/components/friendsList/friends/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Button from "../../button";
 
 function Friends({ friend }) {
     return (
-        <ul>
+        <li>
             <img src={friend.image} alt={friend.name} />
             <h3>{friend.name}</h3>
             {friend.balance < 0 && (
@@ -16,8 +17,8 @@ function Friends({ friend }) {
                 </p>
             )}
             {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-            <button className="select">Select</button>
-        </ul>
+            <Button>Select</Button>
+        </li>
     );
 }
 
