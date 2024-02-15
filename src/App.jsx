95448eca1742b8d002d66d76348c39849a99474a
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-15 08:36:40 +0100

    Added Splitbill and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
+import Button from "./components/button";
 import FriendsList from "./components/friendsList";
-
-const initialFriends = [
-    {
-        id: 118836,
-        name: "Clark",
-        image: "https://i.pravatar.cc/48?u=118836",
-        balance: -7,
-    },
-    {
-        id: 933372,
-        name: "Sarah",
-        image: "https://i.pravatar.cc/48?u=933372",
-        balance: 20,
-    },
-    {
-        id: 499476,
-        name: "Anthony",
-        image: "https://i.pravatar.cc/48?u=499476",
-        balance: 0,
-    },
-];
+import AddFriend from "./components/friendsList/addFriend";
+import SplitBill from "./components/splitBill";
+import "./index.css";
 
 function App() {
     return (
-        <>
-            <FriendsList />
-        </>
+        <div className="app">
+            <div className="sidebar">
+                <FriendsList />
+                <AddFriend />
+                <Button>Add Friend</Button>
+            </div>
+            <SplitBill />
+        </div>
     );
 }
 
