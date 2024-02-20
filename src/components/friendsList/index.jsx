16bd10d9842d8629b3d5e115fcdb20b455189d01
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-20 12:38:08 +0100

    Lifted state up

--- src/components/friendsList/index.jsx
+++ src/components/friendsList/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Friends from "./friends";
 
-function FriendsList({ friends, onSelection }) {
+function FriendsList({ friends, onSelection, selectedFriend }) {
     return (
         <ul>
             {friends.map((friend) => (
                 <Friends
+                    selectedFriend={selectedFriend}
                     friend={friend}
                     key={friend.id}
                     onSelection={onSelection}
